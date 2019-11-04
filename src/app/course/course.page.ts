a788fd81660e20aa9297ce285fef71230f0c3a42
牛育凱 <niuyukai@ushiikugainoMacBook-Pro.local>
2019-11-04 23:40:34 +0900

    add sample main layout

---
+++ src/app/course/course.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course',
+  templateUrl: 'course.page.html',
+  styleUrls: ['course.page.scss']
+})
+export class CoursePage {
+
+  constructor() {}
+
+}
